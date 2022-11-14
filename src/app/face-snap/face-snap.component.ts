a63432e641096daf4f87ce5f927ae687d37bb8ca
Vivien <viendev34@gmail.com>
2022-11-14 14:30:12 +0100

    Ajout propriétés a FaceSnapComponent

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 
 
- /* Décorateur. 
- Apporter des modifications 
- à une classe*/
-  @Component({
-     selector: 'app-face-snap',
-     templateUrl: './face-snap.component.html',
-     styleUrls: ['./face-snap.component.scss']
-   })
+/* Décorateur. 
+Apporter des modifications à une classe*/
+@Component({
+   selector: 'app-face-snap',
+   templateUrl: './face-snap.component.html',
+   styleUrls: ['./face-snap.component.scss']
+})
 
+
+//  Le ! permet de ne pas remonter d'erreur 
+//  pour avoir créé des propriétés sans les initialiser je suis tres bian sa dfzef fef 
 export class FaceSnapComponent {
-
-
+   titel!: string;
+   description!: string;
+   date!: Date;
+   nb_snap!: number;
 }
+
